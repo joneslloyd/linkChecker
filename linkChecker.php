2642665efbce38aa6b69c18b4866700611ae2ada
Lloyd Jones <joneslloyd@users.noreply.github.com>
2018-10-02 14:03:15 +0100

    Update linkChecker.php

--- linkChecker.php
+++ linkChecker.php
@@ -1,46 +1,53 @@
 <html>
- <head>
-  <title>Link Checker</title>
- </head>
- <body>
 
-<?php
+<head>
+    <title>Link Checker</title>
+</head>
+
+<body>
+
+    <?php
 
 $url = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $url = test_input($_POST["url"]);
-  }
-
-function test_input($data) {
-  return $data;
- $html = file_get_contents($url);
-
-$doc = new DOMDocument();
-$doc->loadHTML($html);
-
-$xpath = new DOMXpath($doc);
-
-$nodes = $xpath->query('//a');
-
-foreach($nodes as $node) {
-    var_dump($node->getAttribute('href'));
+    $url = test_input($_POST["url"]);
 }
+
+function test_input($data)
+{
+    return $data;
+    $html = file_get_contents($url);
+
+    $doc = new DOMDocument();
+    $doc->loadHTML($html);
+
+    $xpath = new DOMXpath($doc);
+
+    $nodes = $xpath->query('//a');
+
+    $hrefs = [];
+    foreach ($nodes as $node) {
+        $hrefs[] = $node->getAttribute('href');
+    }
+
+    return $hrefs;
 }
 
 
 
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  URL: <input type="text" name="url">
-  <input type="submit" name="submit" value="Submit">  
-</form>
+    <h2>PHP Form Validation Example</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF"]);?>">
+        URL: <input type="text" name="url">
+        <input type="submit" name="submit" value="Submit">
+    </form>
 
-<?php
+    <?php
 echo "<h2>Your Input:</h2>";
 echo $url;
 ?>
- </body>
+</body>
+
 </html>
